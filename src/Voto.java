cd9e208ea8cf353858426aaee965044d511c4022
maripachon <pachonmaritza102@gmail.com>
2025-04-10 16:23:39 -0500

    Update Voto.java

--- src/Voto.java
+++ src/Voto.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
 public class Voto {
+    private Candidato candidato;
+    private String medio;
+
+    public Voto() {
+        // Constructor vacío
+    }
+
+    public Voto(Candidato candidato, String medio) {
+        this.candidato = candidato;
+        this.medio = medio;
+    }
+
+    public Candidato getCandidato() {
+        return candidato;
+    }
+
+    public void setCandidato(Candidato candidato) {
+        this.candidato = candidato;
+    }
+
+    public String getMedio() {
+        return medio;
+    }
+
+    public void setMedio(String medio) {
+        this.medio = medio;
+    }
 }
+
